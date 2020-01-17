84e5d81cea3e4c632cc9a4c10e69d88409462f42
rayadajani <37849258+rayadajani@users.noreply.github.com>
2020-01-17 14:31:41 -0800

    Add files via upload

---
+++ stranger_things.rb
@@ -0,0 +1,20 @@
+use_bpm 168
+use_synth :dsaw
+live_loop :second do
+  play :c4
+  sleep 0.5
+  play :e4
+  sleep 0.5
+  play :g4
+  sleep 0.5
+  play :b4
+  sleep 0.5
+  play :c5
+  sleep 0.5
+  play :b4
+  sleep 0.5
+  play :g4
+  sleep 0.5
+  play :e4
+  sleep 0.5
+end
